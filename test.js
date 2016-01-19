28e39f100d715d0e81ae43c0fbe32eb32bec0006
PRDeving <pablo.deving@gmail.com>
2016-01-20 00:09:50 +0100

    separated test

---
+++ test.js
@@ -0,0 +1,38 @@
+
+TDB.Init(Test);
+
+function Test(){
+    test = TDB.Select("test");
+
+    var te = 1000000;
+
+    console.log("Testing write");
+    var it = Date.now();
+    for(var x = 0; x < te; x++) (x == 756123)? test.Save("esta es la prueba de fuego") : test.Save("lorem ipsum");
+    console.log(te," entries in ",(Date.now()-it)/1000," seconds");
+
+    setTimeout(function(){
+        console.log("Testing read");
+        it2 = Date.now();
+        var res = test.Search("lorem");
+        console.log(te," iterations and "+res.length+" matches in ",(Date.now()-it2)/1000," seconds");
+    },1000);
+
+    setTimeout(function(){
+        console.log("Testing read single entry");
+        it2 = Date.now();
+        var res = test.Search("ego");
+        console.log(te," iterations and "+res.length+" matches in ",(Date.now()-it2)/1000," seconds");
+        console.log(res);
+    },1000);
+
+    setTimeout(function(){
+        console.log("Testing read single entry with searchFirst");
+        it2 = Date.now();
+        var res = test.SearchFirst("ego");
+        console.log(te," iterations in ",(Date.now()-it2)/1000," seconds");
+        console.log(res);
+    },1000);
+
+
+}
